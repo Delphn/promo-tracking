77fd1b56a091bdab57f2c3782feff9e98bf20b63
Delphin <delphinrukundo10@gmail.com>
2021-01-17 01:34:22 +0100

    get first 20 products

--- size.js
+++ size.js
@@ -7,7 +7,7 @@ const $ = require('cheerio')
 const CronJob = require('cron').CronJob
 const nodemailer = require('nodemailer')
 
-puppeteer.launch({headless: false}).then(async browser => {
+puppeteer.launch({headless: true}).then(async browser => {
   let promoUrl = 'https://www.sizeofficial.fr/homme/chaussures/promo/?maxprice-eur=20&minprice-eur=0&sort=price-low-high'
 
   // let browser = await puppeteer.launch()
@@ -15,23 +15,62 @@ puppeteer.launch({headless: false}).then(async browser => {
 
   await page.goto(promoUrl, {waitUntil: 'networkidle2' })
 
-  let data = await page.evaluate(() => {
-    let items = document.querySelectorAll(".productListItem")
+  const productList = await page.evaluate(() => {
+    let items = document.querySelector("#productListMain").innerHTML
 
-    let productsDetails = []
-
-    items.forEach(item => {
-      const url =  $('.itemImage', item.innerHTML)
-      productsDetails.push(url)
-    })
-
-
-    return productsDetails
+    return items
   })
 
-  console.log(data)
+  let productsDetails = {
+    '0': {},
+    '1': {},
+    '2': {},
+    '3': {},
+    '4': {},
+    '5': {},
+    '6': {},
+    '7': {},
+    '8': {},
+    '9': {},
+    '10': {},
+    '11': {},
+    '12': {},
+    '13': {},
+    '14': {},
+    '15': {},
+    '16': {},
+    '17': {},
+    '18': {},
+    '19': {},
+  }
+  
+  const urls = $('a[data-e2e="plp-productList-link"]', productList)
+  const pricesWas = $('.was span[data-oi-price]', productList)
+  const pricesNow = $('.now span[data-oi-price]', productList)
 
-  debugger
+  // get prices before discount
+  pricesWas.each(el => {
+    productsDetails[el].pricesWas = pricesWas[el].children[0].data
+    // console.log('prices[el].innerHTML: ', pricesWas[el].children[0].data)
+  })
+
+
+  // get prices after discount
+  pricesNow.each(el => {
+    productsDetails[el].priceNow = pricesNow[el].children[0].data
+    // console.log('prices[el].innerHTML: ', prices[el].children[0].data)
+  })
+
+  // get urls
+  urls.each(el => {
+    productsDetails[el].url = urls[el].attribs.href
+    // productsDetails.push({
+      // url: urls[el].attribs.href
+    // })
+  })
+  
+
+  console.log("productsDetails: ", productsDetails)
 
   await browser.close()
 })
